d47bc5a2af78bf938b5d2231aeed24ee2b2577c5
PietroGm <u17211760@utp.edu.pe>
2023-09-05 00:32:13 -0500

    agregamos todo

---
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -0,0 +1,24 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@GetMapping
+	String holaMundo(@RequestParam(defaultValue = "mundo")String nombre, Model model){
+		model.addAttribute("nombre", nombre);
+		return "hola-mundo";
+	}
+
+}
